5fb5b1a8b7e7dd46f8980f8cbbd21e1e4c7994f2
Dave Loose <dave.loose@abacusinsights.com>
2019-12-17 17:04:18 -0500

    added encode_decode workflow

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-zip -r "Password Generator.alfredworkflow" alfred
-cd password_generator
-zip -r "../Password Generator.alfredworkflow" .
+for plist in $(find . -maxdepth 2 -mindepth 2 -name 'info.plist' -type f); do
+    dir=$(dirname "${plist}")
+    name=$(plutil -convert json "${plist}" -o - | jq -r .name | tr -d '/ ')
+    echo -n "Creating ${name} from ${dir}"
+    ( \
+        zip -r "${name}.alfredworkflow" alfred >/dev/null 2>&1; \
+        cd "${dir}"; \
+        zip -r "../${name}.alfredworkflow" . >/dev/null 2>&1 \
+    )
+    echo ...done
+done
---
+++ encode_decode/encode.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import argparse
+import base64
+import sys
+import urllib
+import urlparse
+
+import alfred
+
+
+def wrap(entries):
+    def wrapper(fn):
+        def try_parse(s):
+            try:
+                return str(fn(s))
+            except:
+                return "Error!"
+        return try_parse
+
+    return {key: wrapper(value) for key, value in entries.items()}
+
+
+ENCODERS = wrap({
+    "Base64": base64.standard_b64encode,
+    "Base64 (URL safe)": base64.urlsafe_b64encode,
+    "URL": urllib.quote_plus
+})
+
+DECODERS = wrap({
+    "Base64": base64.standard_b64decode,
+    "Base64 (URL safe)": base64.urlsafe_b64decode,
+    "URL": urlparse.parse_qs
+})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--output-for-alfred", "-a",
+        action  = "store_const",
+        const   = True,
+        default = False,
+        help    = "Write the results as XML for Alfred. If this isn't present, the results are written 1 per line."
+    )
+
+    args, rest = parser.parse_known_args()
+
+    if len(rest) != 2:
+        sys.exit(1)
+
+    cmd, data = rest
+
+    if cmd not in ("encode", "decode"):
+        sys.exit(1)
+
+    fns = ENCODERS if cmd == "encode" else DECODERS
+
+    result = {key: fn(data) for key, fn in fns.items()}
+    if args.output_for_alfred:
+        feedback = alfred.Feedback()
+        for key, value in result.items():
+            feedback.addItem(title=key, subtitle=value, arg=value)
+        feedback.output()
+    else:
+        for key, value in result.items():
+            print "{}\t{}".format(key, value)
